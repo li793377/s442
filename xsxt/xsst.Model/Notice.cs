71b7cf711a5a040fd5ef143dba39ca224a23fd3d
zhibin.li <1023447870@qq.com>
2019-11-19 12:06:50 +0800

    add

--- xsxt/xsst.Model/Notice.cs
+++ xsxt/xsst.Model/Notice.cs
@@ -12,5 +12,25 @@ namespace xsst.Model
         private string title = "";
         private string infor = "";
         private string submitdate = "";
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Infor
+        {
+            get { return infor; }
+            set { infor = value; }
+        }
+        public string Submitdate
+        {
+            get { return submitdate; }
+            set { submitdate = value; }
+        }
     }
 }
