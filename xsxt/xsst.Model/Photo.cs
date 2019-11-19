6f295b29db9e714faa3de4d76c1bb46576fc50dc
zhibin.li <1023447870@qq.com>
2019-11-19 12:08:36 +0800

    add

---
+++ xsxt/xsst.Model/Photo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xsst.Model
+{
+    class Picture
+    {
+        private string id = "";
+        private string photo = "";
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Photo
+        {
+            get { return photo; }
+            set { photo = value; }
+        }
+    }
+}
